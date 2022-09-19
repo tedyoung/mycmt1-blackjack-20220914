77818d965deaea7a2e93822a2d83c46fbd3b9fc6
Ted M. Young <tedyoung@gmail.com>
2022-09-19 12:13:48 -0700

    LAB #7: Write tests to cover all "three sides" of the Ace handling boundary, found and fixed a bug.

--- src/test/java/com/jitterted/ebp/blackjack/HandValueAceTest.java
+++ src/test/java/com/jitterted/ebp/blackjack/HandValueAceTest.java
@@ -19,11 +19,27 @@ public class HandValueAceTest {
     }
 
     @Test
-    public void handWithOneAceAndOtherCardsEqualTo11IsValuedAt1() throws Exception {
+    public void handWithOneAceAndOtherCardsEqualTo11AceIsValuedAt1() throws Exception {
         Hand hand = HandFactory.createHandWithRanksOf("A", "8", "3");
 
         assertThat(hand.value())
                 .isEqualTo(1 + 8 + 3);
+    }
+
+    @Test
+    public void handWithOneAceAndOtherCardsEqualTo12AceIsValuedAt1() throws Exception {
+        Hand hand = HandFactory.createHandWithRanksOf("A", "8", "4");
+
+        assertThat(hand.value())
+                .isEqualTo(1 + 8 + 4);
+    }
+
+    @Test
+    public void handWithOneAceAndOtherCardsEqualTo10AceIsValuedAt11() throws Exception {
+        Hand hand = HandFactory.createHandWithRanksOf("A", "8", "2");
+
+        assertThat(hand.value())
+                .isEqualTo(11 + 8 + 2); // EVIDENT DATA
     }
 
 
