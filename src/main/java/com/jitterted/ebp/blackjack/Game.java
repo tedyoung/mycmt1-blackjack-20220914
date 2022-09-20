b494b9bc4de51c433268c3d9b4f1937496feb6c1
Ted M. Young <tedyoung@gmail.com>
2022-09-20 12:10:12 -0700

    LAB #9: Player wins Blackjack scenario works

--- src/main/java/com/jitterted/ebp/blackjack/Game.java
+++ src/main/java/com/jitterted/ebp/blackjack/Game.java
@@ -216,6 +216,10 @@ public class Game {
     public void playerPushes() {
         playerBalance += playerBetAmount;
     }
+
+    public void playerWinsBlackjack() {
+        playerBalance += playerBetAmount * 2.5;
+    }
 }
 
 
