60395eea575844613794f297d8e22a59dcb31509
Ted M. Young <tedyoung@gmail.com>
2022-09-20 09:46:09 -0700

    TDD: new wallet, when add money, wallet is not empty

--- src/main/java/com/jitterted/ebp/blackjack/Wallet.java
+++ src/main/java/com/jitterted/ebp/blackjack/Wallet.java
@@ -1,7 +1,14 @@
 package com.jitterted.ebp.blackjack;
 
 public class Wallet {
+
+    private boolean isEmpty = true;
+
     public boolean isEmpty() {
-        return true;
+        return isEmpty;
+    }
+
+    public void addMoney(int amount) {
+        isEmpty = false;
     }
 }
--- src/test/java/com/jitterted/ebp/blackjack/WalletTest.java
+++ src/test/java/com/jitterted/ebp/blackjack/WalletTest.java
@@ -13,4 +13,14 @@ public class WalletTest {
         assertThat(wallet.isEmpty())
                 .isTrue();
     }
+
+    @Test
+    void newWalletAddMoneyWalletIsNotEmpty() {
+        Wallet wallet = new Wallet();
+
+        wallet.addMoney(1);
+
+        assertThat(wallet.isEmpty())
+                .isFalse();
+    }
 }
