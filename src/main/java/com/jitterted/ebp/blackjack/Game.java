3073c5ca31dc57ab5108929a3e7d729a390e3505
Ted M. Young <tedyoung@gmail.com>
2022-09-20 12:31:47 -0700

    End of TDD: refactor to GameOutcome enum that calculates bet payoff.

--- src/main/java/com/jitterted/ebp/blackjack/Game.java
+++ src/main/java/com/jitterted/ebp/blackjack/Game.java
@@ -76,20 +76,26 @@ public class Game {
 
         displayFinalGameState();
 
-        determineOutcome(playerBusted);
+        GameOutcome gameOutcome = determineOutcome(playerBusted);
+        playerBalance += gameOutcome.payoffAmount(playerBetAmount);
     }
 
-    private void determineOutcome(boolean playerBusted) {
+    private GameOutcome determineOutcome(boolean playerBusted) {
         if (playerBusted) {
             System.out.println("You Busted, so you lose.  💸");
+            return GameOutcome.PLAYER_LOSES;
         } else if (dealerHand.isBusted()) { // dealerHand.isBusted()
             System.out.println("Dealer went BUST, Player wins! Yay for you!! 💵");
+            return GameOutcome.PLAYER_WINS;
         } else if (playerHand.beats(dealerHand)) { // playerHand.beats(dealerHand)
             System.out.println("You beat the Dealer! 💵");
+            return GameOutcome.PLAYER_WINS;
         } else if (dealerHand.pushes(playerHand)) { // dealerHand.pushes(playerHand)
             System.out.println("Push: You tie with the Dealer. 💸");
+            return GameOutcome.PLAYER_PUSHES;
         } else {
             System.out.println("You lost to the Dealer. 💸");
+            return GameOutcome.PLAYER_LOSES;
         }
     }
 
@@ -205,16 +211,18 @@ public class Game {
         playerBetAmount = betAmount;
     }
 
+    //--- the code below would go away after the refactoring to GameOutcome
+
     public void playerWins() {
         playerBalance += playerBetAmount * 2;
     }
 
     public void playerLoses() {
-
+        playerBalance += playerBetAmount * 0;
     }
 
     public void playerPushes() {
-        playerBalance += playerBetAmount;
+        playerBalance += playerBetAmount * 1;
     }
 
     public void playerWinsBlackjack() {
